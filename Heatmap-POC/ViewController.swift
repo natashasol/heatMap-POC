276867435224561f9c1ac23d689b0412edc46a01
Natasha Solanki <natasha.solanki@credera.com>
2020-02-19 13:23:54 -0600

    first commit

--- Heatmap-POC/ViewController.swift
+++ Heatmap-POC/ViewController.swift
@@ -18,3 +18,25 @@ class ViewController: UIViewController {
 
 }
 
+
+@IBDesignable public class RoundedView: UIView {
+
+    @IBInspectable var borderColor: UIColor = UIColor.white {
+        didSet {
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 2.0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+
+}
